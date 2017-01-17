86f78546aa741e78eec1bb6060147935c60897c4
Axel <lezinaxel@gmail.com>
2017-01-17 15:23:00 +0100

    Ajout du chemin /about

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use App\Twoot;
 Route::get('/', function () {
     // Récupérer tous les "twoots" et les ajouter à la vue
     return view('app')->with([
-        'twoots' => [] //Twoot::___()
+        'twoots' => Twoot::all()
     ]);
 });
 
 //Ici, décommenter et créer la route '/about'
-//Route::get('______', function(){
-//    return ____; //Renvoyer la vue 'about' !
-//});
+Route::get('/about', function(){
+    return view ('about'); //Renvoyer la vue 'about' !
+});
 
 Route::post('twoots', function(){
 //    Twoot::______([
